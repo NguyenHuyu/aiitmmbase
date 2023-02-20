cd03a176d94e2adc6614e1777c7bb4b4d8ae2458
Vu Trung Nam <vutrungnam2002@gmail.com>
2023-02-20 22:26:16 +0700

    fix bg color

--- client/src/components/CardKhoaHoc/CardKhoaHoc.jsx
+++ client/src/components/CardKhoaHoc/CardKhoaHoc.jsx
@@ -27,7 +27,7 @@ return (
     (courses.map((course,index)=>{
       return  (<>
       <div className='mt-[20px] '>
-          <div className=' [background:rgb(55,180,153)] [background:linear-gradient(90deg,rgba(55,180,153,1)_0%,rgba(28,163,190,1)_100%)] h-[100px] rounded-[15px] flex justify-center items-center shadow-md gap-[5px] text-white lg:h-[150px]'>
+          <div className=' bg-gradient-to-r from-sky-500 to-indigo-500 h-[100px] rounded-[15px] flex justify-center items-center shadow-md gap-[5px] text-white lg:h-[150px]'>
               <span className='font-[600]'>{course.title}</span>
               <span className=' font-[600]'>{course.level}</span>
           </div>
